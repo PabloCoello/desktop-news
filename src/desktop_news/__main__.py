e63ade1745671bc711c8e5e05bacab2eafa7c37a
David Coello Pulido <davidcoello@eprosima.com>
2024-08-25 14:59:43 +0200

    Add OpenAI image generator
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

--- src/desktop_news/__main__.py
+++ src/desktop_news/__main__.py
@@ -10,8 +10,7 @@ from datetime import datetime
 from desktop_news.ConfFileManager import generate_conf_file_template
 from desktop_news.PromptBuilder import PromptBuilder
 from desktop_news.Preprocessors.OpenAIPreprocessor import OpenAIPreprocessor
-
-from openai import OpenAI
+from desktop_news.Generators.OpenAIGenerator import OpenAIGenerator
 
 
 parser = argparse.ArgumentParser(
@@ -35,49 +34,23 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def convert_image(image_path, response_path, filename):
-    with open(f"{response_path}/{filename}", mode="r", encoding="utf-8") as file:
-        response = json.load(file)
-    image_data = b64decode(response)
-    image_file = f"{image_path}/{filename}.png"
+def save_image(conf, response):
+    # b = json.loads(response["b64"])
+    image_data = b64decode(response["b64"])
+    filename = f"{datetime.today().date().isoformat()}-{response['created']}.png"
+    image_file = f"{conf['image_path']}/{filename}"
     with open(image_file, mode="+wb") as png:
         png.write(image_data)
-
-
-def save_image(response_path, filename, response):
-    with open(f"{response_path}/{filename}", mode="w", encoding="utf-8") as file:
-        json.dump(response.data[0].b64_json, file)
-
-
-def generate_image(conf, client, prompt, set_wallpaper=False):
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        size='1792x1024',
-        quality="standard",
-        n=1,
-        response_format="b64_json"
-    )
-    filename = f"{datetime.today().date().isoformat()}-{response.created}"
-    save_image(conf["response_path"],  filename, response)
-    convert_image(conf["image_path"], conf["response_path"], filename)
-    if set_wallpaper:
-        # Command to change wallpaper
-        # TODO(dcoello): remove this or change sink actions to generic.
-        path = conf["image_path"]
-        command = f"gsettings set org.gnome.desktop.background picture-uri-dark file://{f'{path}/{filename}.png'}"
-        subprocess.run(command, shell=True)
+        print(image_file)
 
 
 def main():
     if args.generateconf:
         generate_conf_file_template("/".join(args.config.split("/")[:-1]))
         sys.exit(0)
-
     try:
         with open(args.config, "r") as f:
             conf = json.load(f)
-            client = OpenAI(api_key=conf.get("openai_api_key"))
             prompts = conf.get("prompt")
     except FileNotFoundError:
         print("conf file not found, launch with --generate-conf to auto generate it on ~/.config/desktop-news/")
@@ -85,13 +58,13 @@ def main():
 
     builder = PromptBuilder(conf, args)
     preprocess = OpenAIPreprocessor(conf, args)
+    generator = OpenAIGenerator(conf, args)
 
     prompt_content = builder.build_prompt(exclude_tags=["literal"])
     prompt = preprocess.preprocess(prompt_content)
     prompt += builder.build_prompt(include_tags=["literal"])
-
-    print(prompt)
-    generate_image(conf, client, prompt, set_wallpaper=True)
+    res = generator.generate(prompt)
+    save_image(conf, res)
 
 
 if __name__ == "__main__":
