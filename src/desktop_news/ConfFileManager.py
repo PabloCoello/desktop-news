ad44a77c9f9565cb16599a7181d2491abe2e6dff
Pablo Coello <pablocoello@beway.org>
2024-08-26 01:49:59 +0200

    Removed proper nuns to comply with usage policies

--- src/desktop_news/ConfFileManager.py
+++ src/desktop_news/ConfFileManager.py
@@ -7,7 +7,7 @@ CONF_FILE_TEMPLATE = dict(
     nyt_api_key="your nyt api key",
     openai_api_key="yout openai api key",
     prompt=dict(
-        summary="below you will find a series of relevant news of today. could you summarize them and try to combine them into a single text with no more than 4 sentences?, i would also like you make all the necessary modifications to comply with dall-e's usage policies (avoid mention ethnicities, race, violent language and anything that could offend anyone):"
+        summary="below you will find a series of relevant news of today. could you summarize them and try to combine them into a single text with no more than 4 sentences?, i would also like you make all the necessary modifications to comply with dall-e's usage policies (avoid mention ethnicities, race, violent language, proper nouns and surnames and anything that could offend anyone):"
     ),
     response_path="/tmp/",
     image_path="~/images",
